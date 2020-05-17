f8e18394049a8253d9988dc7d05e2efc14bd06e5
mahmoud khalifa <mahmoudbkhalifa@gmail.com>
2020-05-17 23:04:16 +0100

    Updates

---
+++ precache-manifest.a75542819ec271e855deccd6a67d4054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ed4bac1d61644ad23ecd14af32d928",
+    "url": "/facerecognitionapi/index.html"
+  },
+  {
+    "revision": "30ab7e58f5f6760d44c5",
+    "url": "/facerecognitionapi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "416d92fd415c243a4482",
+    "url": "/facerecognitionapi/static/css/main.ef8c5213.chunk.css"
+  },
+  {
+    "revision": "30ab7e58f5f6760d44c5",
+    "url": "/facerecognitionapi/static/js/2.31010643.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionapi/static/js/2.31010643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416d92fd415c243a4482",
+    "url": "/facerecognitionapi/static/js/main.21c4a0e0.chunk.js"
+  },
+  {
+    "revision": "72f5db5903269aa7730c",
+    "url": "/facerecognitionapi/static/js/runtime-main.77bbc8d6.js"
+  }
+]);
